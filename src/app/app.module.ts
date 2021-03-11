c93c4b7834a1a3ad88190858be7747c738a6dfd7
Yishnu Pramanik <yishnupramnaik96@gmail.com>
2021-03-12 00:01:20 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    // HttpClient,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
