17c71710db2c6bba88c33461ddf39216a36069d6
Yishnu Pramanik <yishnupramnaik96@gmail.com>
2021-03-12 01:17:03 +0530

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ export class AppComponent implements OnInit {
   title = 'spaceX';
 
   missions: any[] | undefined;
-  launch: boolean | undefined ;
-  land: boolean | undefined ;
-  loading: boolean = true;
+  launch: string | undefined;
+  land: string | undefined;
+  loading = true;
   year: string | undefined;
 
   constructor( private http: HttpClient ) { }
@@ -22,7 +22,6 @@ export class AppComponent implements OnInit {
   }
 
   getAllPrograms() {
-    console.log( this.land, this.launch );
     this.http.get( 'https://api.spacexdata.com/v3/launches?limit=100' ).subscribe( ( data: any ) => {
       console.log( data );
       this.missions = data;
@@ -31,61 +30,53 @@ export class AppComponent implements OnInit {
   }
 
   launchYear( year: string ) {
-    console.log( year );
-
     this.year = year;
     this.getlandingPrograms();
-    // const options = {
-    //   launch_year: year
-    // };
-    // this.http.get( 'https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=true&amp;', { params: options } ).subscribe( ( data: any ) => {
-    //   this.missions = data;
-    //   console.log( 'year API is called' );
-
-
-    // } )
-    // this.http.get()
   }
 
-  landing( e: boolean ) {
-    this.launch = e;
-    this.getlandingPrograms();
-    // this.http.get( 'https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=' + e ).subscribe( ( data: any ) => {
-    //   this.missions = data;
-    // } );
-  }
-
-  launching( e: boolean ) {
+  landing( e: string ) {
     this.land = e;
     this.getlandingPrograms();
+  }
 
-    // this.http.get( 'https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=' + e ).subscribe( ( data: any ) => {
-    //   this.missions = data;
-    // } )
+  launching( e: string ) {
+    this.launch = e;
+    this.getlandingPrograms();
+  }
+
+  reset() {
+    this.getAllPrograms();
+    this.land = undefined;
+    this.launch = undefined;
+    this.year = undefined;
+
   }
 
   getlandingPrograms() {
+    this.loading = true;
     console.log( this.launch, this.land, this.year );
 
 
     let params = new HttpParams();
-    // params = params.append('launch_success', );
-    // params = params.append('land_success', 10);
-    // params = params.append('launch_year', this.year)
+    if ( this.launch !== undefined ) {
+      params = params.append( 'launch_success', this.launch );
+    }
+    if ( this.land !== undefined ) {
+      params = params.append( 'land_success', this.land );
+    }
+    if ( this.year !== undefined ) {
+      params = params.append( 'launch_year', this.year );
+    }
+
+    console.log( params );
 
 
-    // 'https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&launch_year=2014'
-      // let url = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_year='+ this.year;
-    // let url = 'https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=' + this.launch + '&amp;land_success=' + this.land + '&amp;launch_year=' + this.year;
-    // this.http.get(url).subscribe(( data: any) => {
-    //   console.log(data);
-    //   this.missions = data;
-    // })
+    this.http.get( 'https://api.spaceXdata.com/v3/launches?limit=100', { params } ).subscribe( ( data: any ) => {
+      this.missions = data;
+      console.log( data );
+      this.loading = false;
+    } );
 
-  //  this.http.get('https://api.spaceXdata.com/v3/launches?limit=100',{ launch_success : launch  })
-
-
-    // this.http.get( 'https://api.spacexdata.com/v3/launches?limit=100', { params: options } )
   }
 
 }
